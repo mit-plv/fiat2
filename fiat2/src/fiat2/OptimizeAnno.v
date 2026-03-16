dc940bac4523a19fdc99208ed2d76703727b8239
Xin Zhang <zhangx3@mit.edu>
2026-03-15 23:47:08 -0400

    Add an example using the compilation pipeline with heuristic annotations

--- fiat2/src/fiat2/OptimizeAnno.v
+++ fiat2/src/fiat2/OptimizeAnno.v
@@ -4,6 +4,9 @@ Require Import coqutil.Map.Interface coqutil.Map.SortedListString coqutil.Word.I
 From Stdlib Require Import List String ZArith Permutation.
 Import ListNotations.
 
+Require Import fiat2.Notations.
+Open Scope fiat2_scope.
+
 Variant basic_transf :=
   | ToFilter
   | ToProj
@@ -569,4 +572,52 @@ end.
     eapply mk_idx_related_transfs_sound; eauto.
   Qed.
 
+
+
+  Section Example2.
+    Definition attr2 := "department-id".
+
+    Definition row_ty2_1 := TRecord (record_sort [("department-id", TInt); ("department-name", TString)]).
+    Definition row_ty2_2 := TRecord (record_sort [("name", TString); ("department-id", TInt); ("feedback", TString)]).
+
+    Definition join_tables : expr := ESort LikeList <[
+          "r1" <- mut "departments" ;
+          "r3" <- ("r2" <- mut "responses" ;
+                   check("r2"["department-id"] == "r1"["department-id"]);
+                   ret "r2") ;
+          ret {"name" : "r3" ["name"],
+                "department" : "r1" ["department-name"],
+                  "feedback" : "r3" ["feedback"]} ]>.
+
+    Definition ex2 := CLetMut (EVar "dpt_tbl") "departments"
+                        (CLetMut (EVar "res_tbl") "responses"
+                           (CForeach (EBinop ORange (EAtom (AInt 0)) (EAtom (AInt 10000))) "i"
+                              (CSeq (CAssign "all_feedback" (EAtom (AString "")))
+                                    (CForeach join_tables "r"
+                                     <{ let "name" = "r"["name"] +++ << EAtom (AString " from ") >> in
+                                        let "dep" = "r"["department"] +++ << EAtom (AString " wrote: ") >> in
+                                        let "feedback" = "r"["feedback"] +++ << EAtom (AString "\n") >> in
+                                        let "line" = "name" +++ "dep" +++ "feedback" in
+                                        set "all_feedback" := mut "all_feedback" +++ "line" }>)))).
+    Definition init_Gstore2 : ctenv := map.put map.empty "all_feedback" TString.
+    Definition init_Genv2 : ctenv := map.put (map.put map.empty "res_tbl" (TList row_ty2_2)) "dpt_tbl" (TList row_ty2_1).
+
+    Definition ex2_heu := AC
+                            [PushdownCollection; AnnotateCollection; FilterPushdown; ToFilter; ToProj; ToJoin]
+                            [[DictIdx attr2]; [DictIdx attr2]] ex2.
+
+    Definition ex2_transformed := apply_optimize_anno ex2_heu init_Gstore2 init_Genv2.
+    (* Compute (typecheck init_Gstore2 init_Genv2 ex2). *)
+    Compute ex2_transformed.
+
+    Theorem ex2_transformed_sem : forall (store env : clocals),
+        locals_wf init_Gstore2 store ->
+        locals_wf init_Genv2 env ->
+        interp_command store env ex2_transformed = interp_command store env ex2.
+    Proof.
+      unfold ex2_transformed, apply_optimize_anno. unfold ex2_heu.
+      eapply transf_sound__preserve_sem;
+      eauto using apply_all_transfs_sound with transf_hints.
+    Qed.
+  End Example2.
 End WithConcreteMaps.
